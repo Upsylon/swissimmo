cc8534bc3c305eed24d921ded05e779dcd06485b
Upsylon <patrik.grandadam@unil.ch>
2018-12-17 21:32:35 +0100

    Add shiny app as a function

--- R/immodata.R
+++ R/immodata.R
@@ -650,4 +650,130 @@ my_theme <- function(base_size = 10, base_family = "sans") {
 
 
 
+#' @title Shiny app of the swiss real estate market
+#' @description This function enables to add a beautiful theme to ggplot graphs
+#' @param all_cities A dataframe resulting from the get_immodata2 function,
+#' containing the different housings, their prices, rooms, m2, city, addresses,
+#' longitude, latitude and predicted_price.
+#' @author Germano David
+#' @author Lomazzi Vincent
+#' @author Bron Luca
+#' @author Raisin Edgar
+#' @author Grandadam Patrik
+#' @importFrom magrittr %>%
+#' @export
+#' @examples
+#' my_cities <- get_immodata2("lausanne", "nyon", "geneve")
+#' swissmap(my_cities)
+swissmap <- function(all_cities) {
+
+  ui <- shiny::shinyUI(
+    shiny::fluidPage(
+      leaflet::leafletOutput("map"),
+      shiny::p(),
+      shiny::column(4,
+                    shiny::wellPanel(
+                      shiny::checkboxGroupInput("City", "City",
+                                                sort(as.character(unique(all_cities$city))),
+                                                selected = c("Geneve", "Zurich", "Winterthur", "Lausanne"))
+                    )),
+      shiny::column(8,
+                    shiny::wellPanel(
+                      shiny::sliderInput(
+                        "rangePrice",
+                        "Price",
+                        min(all_cities$price),
+                        max(all_cities$price),
+                        value = range(all_cities$price),
+                        step = 10
+                      ),
+                      shiny::sliderInput(
+                        "rangeRooms",
+                        "Rooms",
+                        min(all_cities$rooms),
+                        max(all_cities$rooms),
+                        value = range(all_cities$rooms),
+                        step = 0.5
+                      ),
+                      shiny::sliderInput(
+                        "rangeM2",
+                        "Surface in m\u00B2",
+                        min(all_cities$m2),
+                        max(all_cities$m2),
+                        value = range(all_cities$m2),
+                        step = 1
+                      )
+                    ))
+    ))
+
+  server <- function(input, output) {
+
+    filteredData <- shiny::reactive({
+      all_cities[all_cities$price >= input$rangePrice[1] &
+                   all_cities$price <= input$rangePrice[2] &
+                   all_cities$rooms >= input$rangeRooms[1] &
+                   all_cities$rooms <= input$rangeRooms[2] &
+                   all_cities$m2 >= input$rangeM2[1] &
+                   all_cities$m2 <= input$rangeM2[2] &
+                   all_cities$city %in% input$City,]
+    })
+
+    pal <- leaflet::colorQuantile(c("green", "#cccccc", "red"),
+                                  all_cities$price/all_cities$predicted_price, n = 4)
+
+    output$map <- leaflet::renderLeaflet({
+      leaflet::leaflet(options = leaflet::leafletOptions(minZoom = 7.4)) %>%
+        leaflet::setMaxBounds(5.5, 48.2, 11, 45.3) %>%
+        leaflet::addTiles() # Add default OpenStreetMap map tiles
+    })
+
+
+    shiny::observe({
+      leaflet::leafletProxy("map", data = filteredData()) %>%
+        leaflet::clearMarkers() %>%
+        leaflet::clearMarkerClusters() %>%
+        leaflet::addCircleMarkers(
+          radius = 10,
+          lng = filteredData()$longitude,
+          lat = filteredData()$latitude,
+          color = "Black",
+          fillColor = pal(filteredData()$price/filteredData()$predicted_price),
+          weight = 2,
+          opacity = 1,
+          fillOpacity = 1,
+          fill = TRUE,
+          popup = paste(
+            "<b>Price :</b>",
+            filteredData()$price,
+            "   CHF",
+            "<br/>",
+            "<b>Predicted price :</b>",
+            filteredData()$predicted_price,
+            " CHF",
+            "<br/>",
+            "<b>Address :</b>",
+            filteredData()$address,
+            "<br/>",
+            "<b>Number of rooms :</b>",
+            filteredData()$rooms,
+            "<br/>",
+            "<b>Size :</b>",
+            filteredData()$m2,
+            " m2"
+          ),
+          clusterOptions = leaflet::markerClusterOptions(disableClusteringAtZoom = 11,
+                                                         spiderfyOnMaxZoom = FALSE)
+        ) #%>%
+      # fitBounds(
+      #   min(filteredData()$longitude),
+      #   min(filteredData()$latitude),
+      #   max(filteredData()$longitude),
+      #   max(filteredData()$latitude)
+      # )
+    })
+  }
+
+  shiny::shinyApp(ui = ui, server = server)
+
+}
 
